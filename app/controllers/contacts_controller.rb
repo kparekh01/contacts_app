da93a384907dfec907cb71fdf3e452a7841f171e
Kairav Parekh <kparekh01@gmail.com>
2016-07-25 21:34:12 -0400

    updated app

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,13 +8,17 @@ class ContactsController < ApplicationController
   end
 
   def create
+    coordinates = Geocoder.coordinates(params[:address])
     @contact = Contact.new(
     first_name: params[:first_name],
     last_name:  params[:last_name],
     phone_number: params[:phone_number],
     email: params[:email]
+    latitude: coordinates[0]
+    longitude: params[:longitude]
     )
     @contact.save
+
     render 'new.html.erb'
   end
 
